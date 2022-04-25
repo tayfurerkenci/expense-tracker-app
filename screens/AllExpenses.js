e74583e1150b33b786bbe12798ec9fb9696912b0
Tayfur Erkenci <terkenci@gmail.com>
2022-04-25 23:25:12 +0300

    navigation setup completed

---
+++ screens/AllExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const AllExpenses = () => {
+    return <Text>AllExpenses Screen</Text>;
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default AllExpenses;
---
+++ screens/ManageExpense.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const ManageExpense = () => {
+    return <Text>ManageExpenses Screen</Text>;
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ManageExpense;
